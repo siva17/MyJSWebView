07314ce0c7db125034aed8efefff6ae82c775bb7
siva17 <siva@siva4u.com>
2014-12-01 20:21:48 -0500

    .

--- MyJSWebView/javascript.js
+++ MyJSWebView/javascript.js
@@ -2,21 +2,18 @@ window.MY = window.MY || {};
 
 MY = (function(w,my){
     my.NATIVEAPI = {};
-      
-	var callBacksFromNative = {};
-    
+    var callBacksFromNative = {};
     var callNativeAPI = function(obj,functionName,config) {
-    
         var argStr = ":";
         if(config) {
             if(config.callback) {
                 var cbID = "cbID" + (+new Date);
                 callBacksFromNative[cbID] = config.callback;
-				config["callbackID"] = cbID;
+                config["callbackID"] = cbID;
             }
             argStr += encodeURIComponent(JSON.stringify(config));
-        }      
-
+        }
+        
         var iframe = document.createElement("IFRAME");
         iframe.setAttribute("src", "native-api" + ":" + obj + ":" + encodeURIComponent(functionName) + argStr);
         document.documentElement.appendChild(iframe);
@@ -27,7 +24,7 @@ MY = (function(w,my){
         MY.NATIVEAPI.retValue = undefined;
         if(ret) return decodeURIComponent(ret);
     };
-      
+
     my.NATIVEAPI.invokeJSCallback = function(cbID,removeAfterExecute,config) {
         var cb = callBacksFromNative[cbID];
         if(removeAfterExecute) delete(callBacksFromNative[cbID]);
@@ -38,7 +35,6 @@ MY = (function(w,my){
     my.NATIVEAPI.registerJSModule = function(obj,methods) {
         w[obj] = {};
         var jsObj = w[obj];
-
         for(var i=0, l=methods.length; i<l; i++) {
             (function (){
                 var method = methods[i];
@@ -49,6 +45,5 @@ MY = (function(w,my){
             })();
         }
     };
-    
-	return my;
+    return my;
 })(window,MY);
